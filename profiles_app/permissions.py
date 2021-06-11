ed70c9d36b92fc839498dc0ed7410ee6e65cdf8d
Panayiotis <panayiotisiacovou94@gmail.com>
2021-06-11 15:05:46 +0300

    Perimissions on Feed API

--- profiles_app/permissions.py
+++ profiles_app/permissions.py
@@ -10,3 +10,12 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+class UpdateOwnState(permissions.BasePermission):
+    """Allow users to edit their own status"""
+
+    def has_object_permission(self,request,view,obj):
+        """Check user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
